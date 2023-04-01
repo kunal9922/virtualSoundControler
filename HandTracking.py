8eaf074ccea8bdd22674d70344adae3166680b4d
Kunal Soni <kunalsoni9200@gmail.com>
2023-04-01 23:01:10 +0530

    frame rates added

--- HandTracking.py
+++ HandTracking.py
@@ -9,6 +9,10 @@ hands = mpHands.Hands()
 
 mpDraw = mp.solutions.drawing_utils
 
+#Frame rates
+pTime = 0
+cTime = 0
+
 while True:
     success, img = cap.read()
     #Convert to rgb image
@@ -20,8 +24,12 @@ while True:
     #taking all hands to draw landmarks individually
     if results.multi_hand_landmarks:
        for handlms in results.multi_hand_landmarks:
-          mpDraw.draw_landmarks(img, handlms)
-
+          mpDraw.draw_landmarks(img, handlms, mpHands.HAND_CONNECTIONS)
+    #count frame rates 
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0),3)
 
     cv2.imshow("Image", img)
     
