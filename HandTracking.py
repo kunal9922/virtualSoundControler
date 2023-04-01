f03a15dfb2a354edc8ef3e4fed8cbb02327409f8
Kunal Soni <kunalsoni9200@gmail.com>
2023-04-01 12:45:23 +0530

    Now being able to detects hand individually and draw landmarks on hands

--- HandTracking.py
+++ HandTracking.py
@@ -4,9 +4,24 @@ import time
 
 
 cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
+mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, img = cap.read()
+    #Convert to rgb image
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img)
+
+    #checking weather hands are detecting 
+    print(results.multi_hand_landmarks)
+    #taking all hands to draw landmarks individually
+    if results.multi_hand_landmarks:
+       for handlms in results.multi_hand_landmarks:
+          mpDraw.draw_landmarks(img, handlms)
+
 
     cv2.imshow("Image", img)
     
