17990e2d833fa07683076a5d9da3a283db3613fd
Kunal Soni <kunalsoni9200@gmail.com>
2023-04-02 14:33:51 +0530

    Converted to Object oriented Code as module from procedural code

--- HandTracking.py
+++ HandTracking.py
@@ -2,49 +2,86 @@ import cv2
 import mediapipe as mp
 import time
 
+class HandDetector():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5) -> None:
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
 
-cap = cv2.VideoCapture(0)
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
+        self.mpHands = mp.solutions.hands
+               
+        self.hands = self.mpHands.Hands(static_image_mode= self.mode, max_num_hands= self.maxHands,
+                                        min_detection_confidence= self.detectionCon, min_tracking_confidence= self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        
 
-mpDraw = mp.solutions.drawing_utils
+    def findHands(self, img, draw=True):
+        #Convert to rgb image
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
 
-#Frame rates
-pTime = 0
-cTime = 0
-
-while True:
-    success, img = cap.read()
-    #Convert to rgb image
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(img)
-
-    #checking weather hands are detecting 
-    print(results.multi_hand_landmarks)
-    #taking all hands to draw landmarks individually
-    if results.multi_hand_landmarks:
-       for handlms in results.multi_hand_landmarks:
-          #finding out each landmarks of tracked hand
-          for id, lm, in enumerate(handlms.landmark):
-             h, w, c = img.shape
-             # we have only decimal values of landmark
-             #to convert it into pixel (landmark.x * widthOfImage) and so on
-             cx, cy = int(lm.x * w) , int(lm.y * h)
-             print(id, cx, cy)
-
-
-          mpDraw.draw_landmarks(img, handlms, mpHands.HAND_CONNECTIONS)
-    #count frame rates 
-    cTime = time.time()
-    fps = 1/(cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0),3)
-
-    cv2.imshow("Image", img)
+        #taking all hands to draw landmarks individually
+        if self.results.multi_hand_landmarks:
+            for handlms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handlms, self.mpHands.HAND_CONNECTIONS)
+        return img
     
-    if cv2.waitKey(25) & 0xFF == ord('q'):                     
-      break
+    def findPosition(self, img, handNo=0, draw = False):
+        lmList = []
+        #finding out each landmarks of tracked hand
+        if self.results.multi_hand_landmarks:
+            hand = self.results.multi_hand_landmarks[handNo]
+            for id, lm, in enumerate(hand.landmark):
+                h, w, c = img.shape
+                # we have only decimal values of landmark
+                #to convert it into pixel (landmark.x * widthOfImage) and so on
+                cx, cy = int(lm.x * w) , int(lm.y * h)
+                #print(id, cx, cy)
+                lmList.append([id,cx,cy])
 
-    cv2.waitKey(1)
+                #draw cricle on the detected landmarks on the hand
+                if draw == True:
+                    cv2.circle(img, (cx,cy), 15, (0,0,255), cv2.FILLED)
+        
+        return lmList
 
-cv2.destroyAllWindows()
+
+def main():
+    
+    #Frame rates
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector()
+
+    while True:
+        success, img = cap.read()
+        #Getting detected hands from the image
+        img = detector.findHands(img, draw=True)
+        
+        #list that contain all the detected land marks of the hand
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            pass
+        #count frame rates 
+        cTime = time.time()
+        fps = 1/(cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0),3)
+
+        cv2.imshow("Image", img)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):                     
+            break
+
+        cv2.waitKey(1)
+
+    cv2.destroyAllWindows()
+    cap.release()
+
+   
+
+if __name__ == "__main__":
+   main()
