bd3e22c7c3953cb8ad825874aa56042f4de140d4
Kunal Soni <kunalsoni9200@gmail.com>
2023-04-07 16:14:34 +0530

    doing hand detection for volume control

--- volumeHandControl.py
+++ volumeHandControl.py
@@ -1,4 +1,25 @@
 import cv2
 import mediapipe
 import numpy as np
+import HandTracking as htm
 
+#------------------ capturing frame size
+wCam, hCam = 640, 480
+#---------------------
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+# Hand tracking module 
+detector = htm.HandDetector(detectionCon=0.7)
+
+while True:
+    success , img = cap.read()
+    img = detector.findHands(img)
+
+    cv2.imshow("Image basic", img)
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):                     
+            break
+    cv2.waitKey(1)
