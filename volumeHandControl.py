f9a026e460dc92c8ad5548851e22ca082f2197b1
Kunal Soni <kunalsoni9200@gmail.com>
2023-04-14 17:04:07 +0530

    Added volume control lib. from pycaw

--- volumeHandControl.py
+++ volumeHandControl.py
@@ -2,6 +2,10 @@ import cv2
 import mediapipe
 import numpy as np
 import HandTracking as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 #------------------ capturing frame size
 wCam, hCam = 640, 480
@@ -14,6 +18,17 @@ cap.set(4, hCam)
 # Hand tracking module 
 detector = htm.HandDetector(detectionCon=0.7)
 
+# control volume library
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+volume.GetMute()
+volume.GetMasterVolumeLevel()
+volume.GetVolumeRange()
+volume.SetMasterVolumeLevel(-20.0, None)
+
+
 while True:
     success , img = cap.read()
     img = detector.findHands(img)
@@ -21,16 +36,23 @@ while True:
     lmList = detector.findPosition(img, draw =False)
     if len(lmList):
         # 4 : THUMB_TIP,  8 : INDEX_FINGER_TIP
-        print(lmList[4], lmList[8])
+        # print(lmList[4], lmList[8])
         # creating circles over the 4 : THUMB_TIP,  8 : INDEX_FINGER_TIP
         x1, y1 = lmList[4][1], lmList[4][2]  # 4 : THUMB_TIP
         x2, y2 = lmList[8][1], lmList[8][2]  # 8 : INDEX_FINGER_TIP
         cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)
         cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
-
+        cx, cy = (x1+x2)//2, (y1+y2)//2
         # creating a line btw 4 : THUMB_TIP AND 8 : INDEX_FINGER_TIP
         cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
-    
+        cv2.circle(img, (cx, cy), 10, (0,255,0), cv2.FILLED)
+        lenght = math.hypot(x2 -x1, y2 - y1)
+        print(lenght)
+
+        if lenght < 50:
+             cv2.cv2.circle(img, (cx, cy), 10, (0,255,0), cv2.FILLED)
+
+
     cv2.imshow("Image basic", img)
 
     if cv2.waitKey(25) & 0xFF == ord('q'):                     
