34189bf148ee47b94333544220957b6556cbbd8c
Kunal Soni <kunalsoni9200@gmail.com>
2023-04-20 14:48:51 +0530

    added GUI toggle switch for hand detection

--- gui.py
+++ gui.py
@@ -1,9 +1,51 @@
 import cv2
 import tkinter as tk
+from tkinter import ttk
 from PIL import Image, ImageTk
+from volumeHandControl import HandVolumeControl 
+import PIL
+
+class Icon:
+    def __init__(self, imgPath, *imgSize: list):
+        self.imgPath = imgPath
+        # Load the icon image
+        icon_image = cv2.imread(self.imgPath)
+
+        if imgSize:
+            self.row, self.col = imgSize[0]
+            icon_image = cv2.resize(icon_image, (self.row, self.col))  # Resize the image if needed
+
+        icon_image = cv2.cvtColor(icon_image, cv2.COLOR_BGR2RGB)
+        icon_image = Image.fromarray(icon_image)  # Replace "icon.png" with the path to your icon image
+        
+        # Create a Tkinter-compatible image
+        self.icon = ImageTk.PhotoImage(icon_image)
+
 
 # OpenCV VideoCapture object for camera input
 cap = cv2.VideoCapture(0)
+hand_volume_control = HandVolumeControl(640, 480)
+
+# by default detection is True 
+detectHand = True
+
+#icon Camera On and OFF
+camOn = Icon(r'./dataset/toggleON.png', (20, 20))
+camOn = camOn.icon
+camOFF = Icon(r'./dataset/toggleOFF.png', (20, 20))
+camOFF = camOFF.icon
+
+
+
+def detectToggle():
+    global detectHand
+    if hd_toggle_button.config('text')[-1] == "ON":
+        hd_toggle_button.config(text="OFF", image =None , compound= tk.RIGHT )
+        detectHand = True
+    else:
+        hd_toggle_button.config(text="ON", image= None, compound= tk.RIGHT )
+        detectHand = False
+
 
 # Function to open the camera
 def open_camera():
@@ -18,11 +60,15 @@ def close_camera():
         cv2.destroyAllWindows()
         cap = None
 
+
 # Function to update the camera frame in the GUI
 def update_frame():
+    global hand_volume_control
     if cap is not None:
         ret, frame = cap.read()
         if ret:
+            if detectHand is not False:
+                frame = hand_volume_control.run(frame)
             # Convert frame from BGR to RGB
             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             # Resize frame to fit in GUI window
@@ -33,24 +79,38 @@ def update_frame():
             # Update label with new frame
             label.config(image=img_tk)
             label.image = img_tk
-    # Schedule next frame update after 10 ms
-    label.after(10, update_frame)
+    # Schedule next frame update after 5 ms
+    label.after(5, update_frame)
 
 # Create the main window
 root = tk.Tk()
 root.title("Camera Viewer")
 
+# Create a frame to hold the camera frame label
+frame = ttk.Frame(root)
+frame.pack(pady=10)
+
 # Create a label to display the camera frame
-label = tk.Label(root)
+label = ttk.Label(frame)
 label.pack()
 
 # Create "Open Camera" button
-open_button = tk.Button(root, text="Open Camera", command=open_camera)
-open_button.pack()
+open_button = ttk.Button(root, text="Open Camera", command=open_camera)
+open_button.pack(pady=5)
 
 # Create "Close Camera" button
-close_button = tk.Button(root, text="Close Camera", command=close_camera)
-close_button.pack()
+close_button = ttk.Button(root, text="Close Camera", command=close_camera)
+close_button.pack(pady=5)
+
+# Create " Hand detection " toggle button
+hd_toggle_button = ttk.Button(root, text="OFF", command= detectToggle)
+hd_toggle_button.pack(pady=5)
+
+
+# Apply some styling to the buttons
+style = ttk.Style()
+style.configure("TButton", padding=10, font=("Helvetica", 12))
+
 
 # Start updating frames
 update_frame()
--- volumeHandControl.py
+++ volumeHandControl.py
@@ -7,63 +7,46 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
-#------------------ capturing frame size
-wCam, hCam = 640, 480
-#---------------------
+class HandVolumeControl:
+    def __init__(self, wCam, hCam):
+       # self.cap = cv2.VideoCapture(0)
+        # self.cap.set(3, wCam)
+        # self.cap.set(4, hCam)
+        self.detector = htm.HandDetector(detectionCon=0.7)
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(
+            IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        self.volume = cast(interface, POINTER(IAudioEndpointVolume))
+        volRange = self.volume.GetVolumeRange()
+        self.minVol = volRange[0]
+        self.maxVol = volRange[1]
 
-cap = cv2.VideoCapture(0)
-cap.set(3, wCam)
-cap.set(4, hCam)
+    def run(self, img):
+        img = self.detector.findHands(img)
+        lmList = self.detector.findPosition(img, draw=False)
+        if len(lmList):
+            x1, y1 = lmList[4][1], lmList[4][2]  
+            x2, y2 = lmList[8][1], lmList[8][2]  
+            cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
+            cx, cy = (x1+x2)//2, (y1+y2)//2
+            cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+            cv2.circle(img, (cx, cy), 10, (0,255,0), cv2.FILLED)
+            length = math.hypot(x2 -x1, y2 - y1)
 
-# Hand tracking module 
-detector = htm.HandDetector(detectionCon=0.7)
+            vol = np.interp(length, [40, 200], [self.minVol, self.maxVol])
+            self.volume.SetMasterVolumeLevel(vol, None)
 
-# control volume library
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = cast(interface, POINTER(IAudioEndpointVolume))
+            if length < 40:
+                cv2.circle(img, (cx, cy), 10, (0,255,0), cv2.FILLED)
 
-# get the range of volume which goes (min -65.25 to max 0.0) 
-volRange = volume.GetVolumeRange()
+        return img 
 
-minVol = volRange[0]
-maxVol = volRange[1]
+    def release(self):
+       # self.cap.release()
+        cv2.destroyAllWindows()
 
-while True:
-    success , img = cap.read()
-    img = detector.findHands(img)
-    #getting the list of detected hands landmakrs postion in pixels
-    lmList = detector.findPosition(img, draw =False)
-    if len(lmList):
-        # 4 : THUMB_TIP,  8 : INDEX_FINGER_TIP
-        # print(lmList[4], lmList[8])
-        # creating circles over the 4 : THUMB_TIP,  8 : INDEX_FINGER_TIP
-        x1, y1 = lmList[4][1], lmList[4][2]  # 4 : THUMB_TIP
-        x2, y2 = lmList[8][1], lmList[8][2]  # 8 : INDEX_FINGER_TIP
-        cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
-        cx, cy = (x1+x2)//2, (y1+y2)//2
-        # creating a line btw 4 : THUMB_TIP AND 8 : INDEX_FINGER_TIP
-        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
-        cv2.circle(img, (cx, cy), 10, (0,255,0), cv2.FILLED)
-        lenght = math.hypot(x2 -x1, y2 - y1)
-       
-
-
-        #hand range 10 - 300
-        #volume range -65 -0
-        '''Now converting the volume range according to pixel line range'''
-        vol = np.interp(lenght, [40, 200], [minVol, maxVol])
-       # print(int(lenght), int(vol))
-        volume.SetMasterVolumeLevel(vol, None)
-        
-        if lenght < 40:
-             cv2.circle(img, (cx, cy), 10, (0,255,0), cv2.FILLED)
-
-
-    cv2.imshow("Image basic", img)
-
-    if cv2.waitKey(25) & 0xFF == ord('q'):                     
-            break
-    cv2.waitKey(1)
+# wCam, hCam = 640, 480
+# hand_volume_control = HandVolumeControl(wCam, hCam)
+# hand_volume_control.run()
+# hand_volume_control.release()
