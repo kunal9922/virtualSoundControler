d112ea4c79a8cb5a6c6601617957773145c4eb27
Kunal Soni <kunalsoni9200@gmail.com>
2023-07-15 13:31:41 +0530

    refactor: Sperate GUI and Main code

--- gui.py
+++ gui.py
@@ -21,36 +21,17 @@ class Icon:
         # Create a Tkinter-compatible image
         self.icon = ImageTk.PhotoImage(icon_image)
 
-# Create the main window
-root = tk.Tk()
-root.title("Wireless Sound Control System")
-ico = Image.open('./dataset/magicHands.png')
-photo = ImageTk.PhotoImage(ico)
-root.wm_iconphoto(False, photo)
 
-
-# OpenCV VideoCapture object for camera input
-cap = cv2.VideoCapture(0)
-hand_volume_control = HandVolumeControl(640, 480)
-
-# by default detection is True 
-detectHand = True
-
-#icon hand detection icon On and OFF
-hndOn = Icon(r'./dataset/toggleON.png', (50, 50))
-hndOn = hndOn.icon
-hndOFF = Icon(r'./dataset/toggleOFF.png', (50, 50))
-hndOFF = hndOFF.icon
 
 
 #toggle switch to Trun ON and Trun OFF hand detection 
 def detectToggle():
-    global detectHand
-    if hd_toggle_button.config('text')[-1] == "OFF":
-        hd_toggle_button.config(text="ON", image = hndOn, compound= tk.RIGHT )
+    global detectHand, app
+    if app.hd_toggle_button.config('text')[-1] == "OFF":
+        app.hd_toggle_button.config(text="ON", image = app.hndOn, compound= tk.RIGHT )
         detectHand = True
     else:
-        hd_toggle_button.config(text="OFF", image = hndOFF, compound= tk.RIGHT )
+        app.hd_toggle_button.config(text="OFF", image = app.hndOFF, compound= tk.RIGHT )
         detectHand = False
 
 
@@ -61,7 +42,7 @@ def open_camera():
 
 # Function to close the camera
 def close_camera():
-    global cap
+    global cap, app
     if cap is not None:
         cap.release()
         cv2.destroyAllWindows()
@@ -71,20 +52,19 @@ def close_camera():
         img = Image.fromarray(imgBlank)
         img_tk = ImageTk.PhotoImage(image=img)
         # Update label with new frame
-        label.config(image=img_tk)
-        label.image = img_tk
-        
-
+        app.label.config(image=img_tk)
+        app.label.image = img_tk
 
 # Function to update the camera frame in the GUI
 def update_frame():
-    global hand_volume_control, scaleValue
+    global hand_volume_control, app
+
     if cap is not None:
         ret, frame = cap.read()
         if ret:
             if detectHand is not False:
                 frame, vol = hand_volume_control.run(frame)
-                scaleValue.set(vol)
+                app.scaleValue.set(vol)
                 
             # Convert frame from BGR to RGB
             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -94,53 +74,81 @@ def update_frame():
             img = Image.fromarray(frame_rgb)
             img_tk = ImageTk.PhotoImage(image=img)
             # Update label with new frame
-            label.config(image=img_tk)
-            label.image = img_tk
+            app.label.config(image=img_tk)
+            app.label.image = img_tk
             
     # Schedule next frame update after 5 ms
-    label.after(5, update_frame)
+    app.label.after(5, update_frame)
+
+class windowGui:
+    def __init__(self):
+        # Create the main window
+        self.root = tk.Tk()
+        self.root.title("Wireless Sound Control System")
+        ico = Image.open('./dataset/magicHands.png')
+        photo = ImageTk.PhotoImage(ico)
+        self.root.wm_iconphoto(False, photo)
+
+    def exe(self):
+        #icon hand detection icon On and OFF
+        self.hndOn = Icon(r'./dataset/toggleON.png', (50, 50))
+        self.hndOn = self.hndOn.icon
+        self.hndOFF = Icon(r'./dataset/toggleOFF.png', (50, 50))
+        self.hndOFF = self.hndOFF.icon
+
+        
+        # Create a frame to hold the camera frame label
+        self.frame = ttk.Frame(self.root)
+        self.frame.pack(pady=10)
+
+        # Create a label to display the camera frame
+        self.label = ttk.Label(self.frame)
+        self.label.pack()
+
+        # Create "Open Camera" button
+        self.open_button = ttk.Button(self.root, text="Open Camera", command=open_camera)
+        self.open_button.pack(pady=5)
+
+        # Create "Close Camera" button
+        self.close_button = ttk.Button(self.root, text="Close Camera", command=close_camera)
+        self.close_button.pack(pady=5)
+
+        # Create " Hand detection " toggle button
+        self.hd_toggle_button = ttk.Button(self.root, text="ON",image=self.hndOn, command= detectToggle)
+        self.hd_toggle_button.pack(pady=5)
+
+        #slider that shows the current volume by changing hand gestures.
+        self.scaleValue = tk.IntVar()
+        slider = ttk.Scale(
+            self.root,
+            from_=0,
+            to=100,
+            orient='horizontal',
+            variable= self.scaleValue
+        )
 
 
-# Create a frame to hold the camera frame label
-frame = ttk.Frame(root)
-frame.pack(pady=10)
-
-# Create a label to display the camera frame
-label = ttk.Label(frame)
-label.pack()
-
-# Create "Open Camera" button
-open_button = ttk.Button(root, text="Open Camera", command=open_camera)
-open_button.pack(pady=5)
-
-# Create "Close Camera" button
-close_button = ttk.Button(root, text="Close Camera", command=close_camera)
-close_button.pack(pady=5)
-
-# Create " Hand detection " toggle button
-hd_toggle_button = ttk.Button(root, text="ON",image=hndOn, command= detectToggle)
-hd_toggle_button.pack(pady=5)
-
-#slider that shows the current volume by changing hand gestures.
-scaleValue = tk.IntVar()
-slider = ttk.Scale(
-    root,
-    from_=0,
-    to=100,
-    orient='horizontal',
-    variable= scaleValue
-)
+        # Apply some styling to the buttons
+        self.style = ttk.Style()
+        self.style.configure("TButton", padding=10, font=("Helvetica", 12))
 
 
-# Apply some styling to the buttons
-style = ttk.Style()
-style.configure("TButton", padding=10, font=("Helvetica", 12))
+        # Start updating frames
+        update_frame()
 
+        # Run the GUI event loop
+        self.root.mainloop()
 
-# Start updating frames
-update_frame()
+# by default detection is True 
+detectHand = True
 
-# Run the GUI event loop
-root.mainloop()
+# OpenCV VideoCapture object for camera input
+cap = cv2.VideoCapture(0)
+hand_volume_control = HandVolumeControl(640, 480)
 
-
+def exe():
+    app = windowGui()
+    # Set the local variable as global
+    globals()["app"] = app
+    app.exe()
+    
---
+++ main.py
@@ -0,0 +1,4 @@
+from gui import exe as gestureVolumeContol
+
+if __name__ == "__main__":
+    gestureVolumeContol.exe()
